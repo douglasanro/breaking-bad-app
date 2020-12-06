674945bb7f820de6dc61669c0fdf1448f5e0bb0f
Douglas Rosa <douglasanro@gmail.com>
2020-12-06 18:31:42 -0300

    test: move mocks to global variable

--- src/stores/characters/charactersActions.test.ts
+++ src/stores/characters/charactersActions.test.ts
@@ -1,58 +1,12 @@
 import charactersReducers, { charactersInitialState } from './charactersReducers';
 import { setCharacters, clearCharacters, setHasNextPage, setFilter, setSearchTerm } from './charactersActions';
 
-const mockCharacters = [
-    {
-        "char_id": 1,
-        "name": "Walter White",
-        "birthday": "09-07-1958",
-        "occupation": [
-            "High School Chemistry Teacher",
-            "Meth King Pin"
-        ],
-        "img": "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-        "status": "Presumed dead",
-        "nickname": "Heisenberg",
-        "appearance": [
-            1,
-            2,
-            3,
-            4,
-            5
-        ],
-        "portrayed": "Bryan Cranston",
-        "category": "Breaking Bad",
-        "better_call_saul_appearance": []
-    },
-    {
-        "char_id": 2,
-        "name": "Jesse Pinkman",
-        "birthday": "09-24-1984",
-        "occupation": [
-            "Meth Dealer"
-        ],
-        "img": "https://vignette.wikia.nocookie.net/breakingbad/images/9/95/JesseS5.jpg/revision/latest?cb=20120620012441",
-        "status": "Alive",
-        "nickname": "Cap n' Cook",
-        "appearance": [
-            1,
-            2,
-            3,
-            4,
-            5
-        ],
-        "portrayed": "Aaron Paul",
-        "category": "Breaking Bad",
-        "better_call_saul_appearance": []
-    }
-];
-
 test('should set characters correctly', () => {
   const before = charactersInitialState;
-  const action = setCharacters.success(mockCharacters);
+  const action = setCharacters.success((global as any).mockCharacters);
   const after = {
     ...charactersInitialState,
-    list: mockCharacters
+    list: (global as any).mockCharacters
   }
 
   expect(charactersReducers(before, action)).toEqual(after);
--- src/stores/characters/charactersThunks.test.ts
+++ src/stores/characters/charactersThunks.test.ts
@@ -12,52 +12,6 @@ type DispatchMock = ThunkDispatch<rootState, void, AnyAction>;
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-const mockCharacters = [
-    {
-        "char_id": 1,
-        "name": "Walter White",
-        "birthday": "09-07-1958",
-        "occupation": [
-            "High School Chemistry Teacher",
-            "Meth King Pin"
-        ],
-        "img": "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-        "status": "Presumed dead",
-        "nickname": "Heisenberg",
-        "appearance": [
-            1,
-            2,
-            3,
-            4,
-            5
-        ],
-        "portrayed": "Bryan Cranston",
-        "category": "Breaking Bad",
-        "better_call_saul_appearance": []
-    },
-    {
-        "char_id": 2,
-        "name": "Jesse Pinkman",
-        "birthday": "09-24-1984",
-        "occupation": [
-            "Meth Dealer"
-        ],
-        "img": "https://vignette.wikia.nocookie.net/breakingbad/images/9/95/JesseS5.jpg/revision/latest?cb=20120620012441",
-        "status": "Alive",
-        "nickname": "Cap n' Cook",
-        "appearance": [
-            1,
-            2,
-            3,
-            4,
-            5
-        ],
-        "portrayed": "Aaron Paul",
-        "category": "Breaking Bad",
-        "better_call_saul_appearance": []
-    }
-];
-
 const middleware = [thunk];
 const mockStore = configureMockStore<{ characters: ICharacterState }, DispatchMock>(middleware);
 let store: ReturnType<typeof mockStore>;
@@ -73,7 +27,7 @@ test('should get characters with success', () => {
     { type: "@characters/FETCH_CHARACTERS_REQUEST" },
   ];
   mockedAxios.get.mockReturnValue({
-    data: mockCharacters,
+    data: (global as any).mockCharacters,
     status: 200
   } as any);
   store.dispatch(getCharacters({ limit: 10 }));
